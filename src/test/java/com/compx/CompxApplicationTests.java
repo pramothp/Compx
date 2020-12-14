f30880153f24e3278716c2cadbd227abd1eb547e
pramothprabha <pramoth74@gmail.com>
2020-12-14 12:51:35 +0530

    Adding change for moving out of spring service folder

---
+++ src/test/java/com/compx/CompxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
